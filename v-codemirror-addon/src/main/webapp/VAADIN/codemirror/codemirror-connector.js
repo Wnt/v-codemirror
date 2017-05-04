56e710a502c49489f8585b015ebad6f296fcd3cc
Jonni Nakari <jonni@vaadin.com>
2017-05-04 23:21:15 +0200

    Change to use connector alias instead of this

--- v-codemirror-addon/src/main/webapp/VAADIN/codemirror/codemirror-connector.js
+++ v-codemirror-addon/src/main/webapp/VAADIN/codemirror/codemirror-connector.js
@@ -1,29 +1,29 @@
 window.org_vaadin_addon_codemirror_CodeMirrorField = function() {
-	var e = this.getElement();
 	var connector = this;
+	var e = connector.getElement();
 	var config = {
-		value : this.getState().value,
-		mode : this.getState().mode,
-		indentUnit : this.getState().indentUnit,
-	    lineNumbers: this.getState().lineNumbers,
-	    lineWrapping: this.getState().lineWrapping,
-	    foldGutter: this.getState().foldGutter,
-	    gutters: this.getState().gutters,
+		value : connector.getState().value,
+		mode : connector.getState().mode,
+		indentUnit : connector.getState().indentUnit,
+	    lineNumbers: connector.getState().lineNumbers,
+	    lineWrapping: connector.getState().lineWrapping,
+	    foldGutter: connector.getState().foldGutter,
+	    gutters: connector.getState().gutters,
 	};
 	// previous synced value
-	this.value = this.getState().value;
-	this.internalValueChange = false;
+	connector.value = connector.getState().value;
+	connector.internalValueChange = false;
 	
-	this.cm = new CodeMirror(e, config);
-	this.valuePropagationTimeout = null;
-	this.sizeCheckTimeout = null;
+	connector.cm = new CodeMirror(e, config);
+	connector.valuePropagationTimeout = null;
+	connector.sizeCheckTimeout = null;
 
 	// workaround to react to the new size after Vaadin's layouting phase
 	sizeCheckTimeout = window.setTimeout(function() {
 		connector.checkSize();
 	}, 200);
 	
-	this.autoFoldImports = function () {
+	connector.autoFoldImports = function () {
 		for (var i = 0; i < connector.cm.lastLine(); ++i) {
 			if (connector.cm.getLine(i).trim().lastIndexOf("import", 0) === 0) {
 			    connector.cm.foldCode(CodeMirror.Pos(i, 0));
@@ -34,7 +34,7 @@ window.org_vaadin_addon_codemirror_CodeMirrorField = function() {
 	
 	if (config.mode === "text/x-java") connector.autoFoldImports();
 
-	this.cm.on("changes", function() {
+	connector.cm.on("changes", function() {
 		if (connector.internalValueChange) {
 			return;
 		}
@@ -48,13 +48,13 @@ window.org_vaadin_addon_codemirror_CodeMirrorField = function() {
 		}, 200);
 	});
 
-	this.checkSize = function () {
+	connector.checkSize = function () {
 		if (connector.cm.display.lastWrapHeight != connector.cm.display.wrapper.clientHeight) {
 			connector.cm.refresh();
 		}
 	};
-	this.onStateChange = function() {
-		var state = this.getState();
+	connector.onStateChange = function() {
+		var state = connector.getState();
 		var oldValue = connector.value;
 		if (oldValue != state.value) {
 			connector.internalValueChange = true;
